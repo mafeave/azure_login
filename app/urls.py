613fcd8cebc98634278737473aced820ed92587c
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2025-06-14 04:58:49 -0500

    Redirect to login

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_successful,name='login-view'),
+    path('login', views.login_successful,name='login-view'),
+    path('', views.register_user, name='register'),
     path('register/', views.register_user, name='register'),
-
 ]
